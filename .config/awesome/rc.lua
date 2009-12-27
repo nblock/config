4b25aa0bfe1c06093f80c08ae44598afc748c860
nblock <nblock@archlinux.us>
2009-12-27 09:35:18 +0100

    removed some rules

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -366,15 +366,6 @@ awful.rules.rules = {
                      focus = true,
                      keys = clientkeys,
                      buttons = clientbuttons } },
-    { rule = { class = "MPlayer" },
-      properties = { floating = true } },
-    { rule = { class = "pinentry" },
-      properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
 }
 -- }}}
 
