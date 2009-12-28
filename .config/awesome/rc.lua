56c1a08c527263c038c431b2f2efe6c79bdb6f6e
nblock <nblock@archlinux.us>
2009-12-28 13:47:47 +0100

    removed lua prompt

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -292,13 +292,6 @@ globalkeys = awful.util.table.join(
     -- Prompt
     awful.key({ modkey },            "r",     function () promptbox[mouse.screen]:run() end),
 
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run({ prompt = "Run Lua code: " },
-                  mypromptbox[mouse.screen].widget,
-                  awful.util.eval, nil,
-                  awful.util.getdir("cache") .. "/history_eval")
-              end)
 )
 
 clientkeys = awful.util.table.join(
