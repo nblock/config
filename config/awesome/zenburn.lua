6432e1a2a63697962430a4ad981b646b9df3309f
Florian Preinstorfer <nblock@archlinux.us>
2012-10-03 18:24:38 +0200

    refactor to new directory

---
+++ config/awesome/zenburn.lua
@@ -0,0 +1,144 @@
+-------------------------------
+--  "Zenburn" awesome theme  --
+--    By Adrian C. (anrxc)   --
+-------------------------------
+
+-- Alternative icon sets and widget icons:
+--  * http://awesome.naquadah.org/wiki/Nice_Icons
+
+-- {{{ Main
+theme = {}
+theme.confdir       = awful.util.getdir("config")
+theme.wallpaper_cmd = { "awsetbg /home/flo/bilder/wallpaper/1440x900/arch_wall-by_kpolicano-1440x900.jpg" }
+-- }}}
+
+-- {{{ Styles
+theme.font      = "sans 8"
+
+-- {{{ Colors
+theme.fg_normal = "#DCDCCC"
+theme.fg_focus  = "#00FF00"
+theme.fg_urgent = "#CC9393"
+theme.bg_normal = "#3F3F3F"
+theme.bg_focus  = "#1E2320"
+theme.bg_urgent = "#3F3F3F"
+-- }}}
+
+-- {{{ Borders
+theme.border_width  = "1"
+theme.border_normal = "#3F3F3F"
+theme.border_focus  = "#FF0000"
+theme.border_marked = "#CC9393"
+-- }}}
+
+-- {{{ Titlebars
+theme.titlebar_bg_focus  = "#3F3F3F"
+theme.titlebar_bg_normal = "#3F3F3F"
+theme.titlebar_widget_height = "16"
+-- }}}
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- titlebar_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- Example:
+--theme.taglist_bg_focus = "#CC9393"
+-- }}}
+
+-- {{{ Widgets
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.fg_widget        = "#AECF96"
+--theme.fg_center_widget = "#88A175"
+--theme.fg_end_widget    = "#FF5656"
+--theme.bg_widget        = "#494B4F"
+--theme.border_widget    = "#3F3F3F"
+-- }}}
+
+-- {{{ Mouse finder
+theme.mouse_finder_color = "#CC9393"
+-- mouse_finder_[timeout|animate_timeout|radius|factor]
+-- }}}
+
+-- {{{ Menu
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_height = "16"
+theme.menu_width  = "100"
+-- }}}
+
+-- {{{ Icons
+-- {{{ Taglist
+theme.taglist_squares_sel   = "/usr/share/awesome/themes/zenburn/taglist/squarefz.png"
+theme.taglist_squares_unsel = "/usr/share/awesome/themes/zenburn/taglist/squarez.png"
+--theme.taglist_squares_resize = "false"
+-- }}}
+
+-- {{{ Misc
+theme.awesome_icon           = "/usr/share/awesome/themes/zenburn/awesome-icon.png"
+theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
+theme.tasklist_floating_icon = "/usr/share/awesome/themes/default/tasklist/floatingw.png"
+-- }}}
+
+-- {{{ Layout icons
+theme.layout_tile       = theme.confdir .. "/icons/layouts/tile.png"
+theme.layout_tileleft   = theme.confdir .. "/icons/layouts/tileleft.png"
+theme.layout_tilebottom = theme.confdir .. "/icons/layouts/tilebottom.png"
+theme.layout_tiletop    = theme.confdir .. "/icons/layouts/tiletop.png"
+theme.layout_fairv      = theme.confdir .. "/icons/layouts/fairv.png"
+theme.layout_fairh      = theme.confdir .. "/icons/layouts/fairh.png"
+theme.layout_spiral     = theme.confdir .. "/icons/layouts/spiral.png"
+theme.layout_dwindle    = theme.confdir .. "/icons/layouts/dwindle.png"
+theme.layout_max        = theme.confdir .. "/icons/layouts/max.png"
+theme.layout_fullscreen = theme.confdir .. "/icons/layouts/fullscreen.png"
+theme.layout_magnifier  = theme.confdir .. "/icons/layouts/magnifier.png"
+theme.layout_floating   = theme.confdir .. "/icons/layouts/floating.png"
+-- }}}
+
+-- {{{ Widget icons
+theme.widget_cpu    = theme.confdir .. "/icons/taskbar/cpu.png"
+theme.widget_bat    = theme.confdir .. "/icons/taskbar/bat.png"
+theme.widget_mem    = theme.confdir .. "/icons/taskbar/mem.png"
+theme.widget_fs     = theme.confdir .. "/icons/taskbar/disk.png"
+theme.widget_net    = theme.confdir .. "/icons/taskbar/down.png"
+theme.widget_netup  = theme.confdir .. "/icons/taskbar/up.png"
+theme.widget_wifi   = theme.confdir .. "/icons/taskbar/wifi.png"
+theme.widget_mail   = theme.confdir .. "/icons/taskbar/mail.png"
+theme.widget_vol    = theme.confdir .. "/icons/taskbar/vol.png"
+theme.widget_org    = theme.confdir .. "/icons/taskbar/cal.png"
+theme.widget_date   = theme.confdir .. "/icons/taskbar/time.png"
+theme.widget_crypto = theme.confdir .. "/icons/taskbar/crypto.png"
+theme.widget_sep    = theme.confdir .. "/icons/taskbar/separator.png"
+-- }}}
+
+-- {{{ Titlebar icons
+theme.titlebar_close_button_focus  = theme.confdir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = theme.confdir .. "/icons/titlebar/close_normal.png"
+
+theme.titlebar_ontop_button_focus_active    = theme.confdir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active   = theme.confdir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = theme.confdir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = theme.confdir .. "/icons/titlebar/ontop_normal_inactive.png"
+
+theme.titlebar_sticky_button_focus_active    = theme.confdir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active   = theme.confdir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = theme.confdir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = theme.confdir .. "/icons/titlebar/sticky_normal_inactive.png"
+
+theme.titlebar_floating_button_focus_active    = theme.confdir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active   = theme.confdir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = theme.confdir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = theme.confdir .. "/icons/titlebar/floating_normal_inactive.png"
+
+theme.titlebar_maximized_button_focus_active    = theme.confdir .. "/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active   = theme.confdir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.confdir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme.confdir .. "/icons/titlebar/maximized_normal_inactive.png"
+-- }}}
+-- }}}
+
+return theme
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+#set -x
+
+#link files
+for item in `find $PWD -maxdepth 1 -mindepth 1 -type f \( ! -iname "install.sh" ! -iname "README.markdown" \) -printf "%P\n"`;do
+    ln -snf "$PWD/$item" "$HOME/.$item"
+done
+
+#process .config
+for item in `find "$PWD/config" -maxdepth 1 -mindepth 1 -printf "%P\n"`;do
+    ln -snf "$PWD/config/$item" "$HOME/.config/$item"
+done
+
+#solarized-task
+ln -snf "$PWD/solarized-task" "$HOME/.solarized-task"
+
+#bin
+ln -snf "$PWD/bin" "$HOME/bin"
+
+#cmus
+ln -snf "$PWD/cmus" "$HOME/.cmus"
+
+#change login shell to zsh
+if ! grep -q "^$USER:.*zsh$" /etc/passwd; then
+    echo "Changing login shell for $USER to zsh ..."
+    chsh -s `which zsh`
+fi
